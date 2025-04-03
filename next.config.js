f95df3b1a8e5e8b65d6fbfcb576c19839d25b62d
tagryu <tagryu22@gmail.com>
2025-04-03 19:38:06 +0900

    Fix: Add transpilePackages option for Radix UI in next.config.js

--- next.config.js
+++ next.config.js
@@ -3,6 +3,12 @@ const nextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
+  transpilePackages: [
+    '@radix-ui/react-checkbox',
+    '@radix-ui/react-label',
+    '@radix-ui/react-slot',
+    '@radix-ui/react-form'
+  ],
 };
 
 module.exports = nextConfig; 
