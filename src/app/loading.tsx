03d3a66823fb86d0613f0a25f0a0ad5956c2968f
tagryu <tagryu22@gmail.com>
2025-04-03 22:19:13 +0900

    haha

---
+++ src/app/loading.tsx
@@ -0,0 +1,8 @@
+export default function Loading() {
+  return (
+    <div className="flex justify-center items-center h-screen">
+      <div className="animate-spin rounded-full h-12 w-12 border-t-2 border-b-2 border-red-600"></div>
+      <p className="ml-2 text-lg text-gray-700">로딩 중...</p>
+    </div>
+  );
+} 
